fb2afa772aaf63a1b7eb423d218aa4d3bf3c8b8b
Sami Mäkelä <mrsmkl@gmail.com>
2024-05-27 13:34:28 +0300

    update

--- wasienv/sdk.py
+++ wasienv/sdk.py
@@ -25,7 +25,7 @@ WASI_SWIFT_ENV_DIR = os.path.join(WASI_SWIFT_DIR, "env")
 # The dir where swiftenv lives
 WASI_SWIFTENV_DIR = os.path.join(WASI_SWIFT_DIR, "swiftenv")
 
-CURRENT_SDK = "11"
+CURRENT_SDK = "16"
 
 SDKS = {
     "5": {
@@ -70,11 +70,18 @@ SDKS = {
         },
         "sysroot": "wasi-sdk-11.0"
     },
+    "16": {
+        "download_urls": {
+            "darwin": 'https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-16/wasi-sdk-16.0-macos.tar.gz',
+            "linux": 'https://github.com/WebAssembly/wasi-sdk/releases/download/wasi-sdk-16/wasi-sdk-16.0-linux.tar.gz',
+        },
+        "sysroot": "wasi-sdk-16.0"
+    },
 }
 
 SDK_TAGS = {
-    "latest": "11",
-    "unstable": "11"
+    "latest": "16",
+    "unstable": "16"
 }
 
 SWIFTWASM = {
